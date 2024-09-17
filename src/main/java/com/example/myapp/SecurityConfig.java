0c896765da1cfc1e7e609b354b9b6d2343b80df7
chris <krzysiek002@gmail.com>
2024-09-17 20:31:16 +0200

    test1

---
+++ src/main/java/com/example/myapp/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.example.myapp;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .requestMatchers("/listUrls").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+        return http.build();
+    }
+
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build());
+        return manager;
+    }
+}
