33f0c2fc898984f8214cd062909c969b7513b9f6
chris <krzysiek002@gmail.com>
2024-11-26 15:12:02 +0100

    changed UrlShortenerController to accept json as request param and return json response

---
+++ src/main/java/com/example/myapp/UrlRequest.java
@@ -0,0 +1,18 @@
+package com.example.myapp;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public class UrlRequest {
+    @NotBlank(message = "URL is mandatory")
+    @Pattern(regexp = "^(https?:\\/\\/)?([\\da-z.-]+)\\.([a-z.]{2,6})([\\/\\w .-]*)*\\/?$", message = "Invalid URL")
+//    @Pattern(regexp = "^(https?:\\\\/\\\\/)?([\\\\da-z.-]+)\\\\.([a-z.]{2,6})([\\\\/\\\\w .-]*)*\\\\/?$", message = "Invalid URL")
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
--- src/main/java/com/example/myapp/UrlShortenerController.java
+++ src/main/java/com/example/myapp/UrlShortenerController.java
@@ -1,19 +1,22 @@
 package com.example.myapp;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import jakarta.servlet.http.HttpServletResponse;
 //import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
+@CrossOrigin(origins = "http://localhost:5173")
 public class UrlShortenerController {
     @Autowired
     private UrlService urlService;
@@ -23,13 +26,20 @@ public class UrlShortenerController {
     }
 
     @PostMapping("/shorten")
-    public String shortenUrl(@RequestParam("url") @Pattern(regexp = "^(https?:\\/\\/)?([\\da-z.-]+)\\.([a-z.]{2,6})([\\/\\w .-]*)*\\/?$") String longUrl, Model model) {
-        String shortUrl = urlService.createShortUrl(longUrl);
-        // Pass both original and short URL to the view
-        model.addAttribute("longUrl", longUrl);
-        model.addAttribute("shortUrl", shortUrl);
-        return "result";  // Returns a page that shows the shortened URL (e.g., result.html)
+    public ResponseEntity<UrlResponse> shortenUrl(@RequestBody @Valid UrlRequest urlRequest) {
+        String shortUrl = urlService.createShortUrl(urlRequest.getUrl());
+        System.out.println(urlRequest.getUrl());
+        System.out.println(shortUrl);
+        return ResponseEntity.ok(new UrlResponse(shortUrl));
     }
+//    @PostMapping("/shorten") method with query parameter returns view with result
+//    public String shortenUrl(@RequestParam("url") @Pattern(regexp = "^(https?:\\/\\/)?([\\da-z.-]+)\\.([a-z.]{2,6})([\\/\\w .-]*)*\\/?$") String longUrl, Model model) {
+//        String shortUrl = urlService.createShortUrl(longUrl);
+//        // Pass both original and short URL to the view
+//        model.addAttribute("longUrl", longUrl);
+//        model.addAttribute("shortUrl", shortUrl);
+//        return "result";  // Returns a page that shows the shortened URL (e.g., result.html)
+//    }
 
     @GetMapping("/{shortUrl}")
     public void redirectToOriginalUrl(@PathVariable String shortUrl, HttpServletResponse response) throws IOException {
